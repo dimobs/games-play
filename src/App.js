b8c366c08c1e4b3b6684db94cc8a3672334688d7
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-02-12 21:44:19 +0200

    Add error page

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
-import { createElement, useState } from 'react';
+import { useState } from 'react';
 import Header from './components/Header';
 import WelcomeWorld from './components/WelcomeWorld';
-import GameCatalog from './components/GameCatalog';
 import GameCreate from './components/GameCreate';
+import GameCatalog from './components/GameCatalog';
+import Login from './components/Login';
+import Register from './components/Register';
+import ErrorPage from './components/404';
+//import Logout from './components/Logout';
+
 
 
 function App() {
-const [page, setPage] = useState('/home');
+    const [page, setPage] = useState('/home');
 
     const routes = {
         '/home': <WelcomeWorld />, //резулт. на комп.(<WelcomeWorld />), а не реф-a (WelcomeWorld)
         '/games': <GameCatalog />,
-        '/create-game': <GameCreate />
+        '/create-game': <GameCreate />,
+        '/login': <Login />,
+        '/register': <Register />,
+
+       // '/logout': <Logout />,
     };
 
     const navigationChangeHandler = (path) => {
         setPage(path);
     }
 
-      return (
-    <div id="box">
+    return (
+        <div id="box">
+            <Header
+                navigationChangeHandler={navigationChangeHandler}
+            />
+            <main id="main-content">
+                {routes[page] || <ErrorPage />}
+            </main>
 
-<Header
-navigationChangeHandler={navigationChangeHandler}
-/>
-<WelcomeWorld />
-
-    <main id="main-content">
- {routes[page] || <h2> no page found!</h2>}
-    </main>
- 
-</div>
+        </div>
     );
 }
 export default App;
