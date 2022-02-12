68e271e86906dddf2062d7998da97bdce7bfa4ca
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-02-12 15:48:35 +0200

    initial commited

---
+++ src/tempo.js
@@ -0,0 +1,22 @@
+function App() {
+    return (
+      <div id="box">
+
+      <section id="catalog-page">
+          <h1>All Games</h1>
+      
+          <div className="allGames">
+              <div className="allGames-info">
+                  <img src="./images/avatar-1.jpg" alt="das" />
+                  <h6>Action</h6>
+                  <h2>MineCraft</h2>
+                  <a href="/dsa/sad">Details</a>
+              </div>
+          </div>
+  
+      </section>
+  </div>
+      );
+  }
+  export default App;
+  
