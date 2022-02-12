cc1e9a1c6b7f5485e96dc5b2ae6a8bfbc7c50f02
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-02-12 20:33:46 +0200

    add routes and components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import Header from './components/Header';
 import WelcomeWorld from './components/WelcomeWorld';
+import GameCatalog from './components/GameCatalog';
+import GameCreate from './components/GameCreate';
 
 
 function App() {
+
+    const routes = {
+        '/home': WelcomeWorld,
+        '/games': GameCatalog,
+        '/create-game': GameCreate
+    }
+
+
   return (
     <div id="box">
 
