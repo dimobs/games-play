f5f9c369e9affea2e8a2e0e09037753714d744d6
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-02-15 19:40:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ return routes[rootPath]
                 navigationChangeHandler={navigationChangeHandler}
             />
             <main id="main-content">
-                {router(page)  || <ErrorPage />}
+            <Route />
             </main>
 
         </div>
